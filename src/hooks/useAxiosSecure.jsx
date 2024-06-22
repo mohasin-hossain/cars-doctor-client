6301832ae5b2835aa1dee0d34e95a1f42ddccd29
Mohasin Hossain <mohasinhossainrajib2@gmail.com>
2024-06-22 08:55:31 +0600

    Axios interceptor to handle response error

--- src/hooks/useAxiosSecure.jsx
+++ src/hooks/useAxiosSecure.jsx
@@ -1,4 +1,7 @@
 import axios from "axios";
+import { useEffect } from "react";
+import useAuth from "./useAuth";
+import { useNavigate } from "react-router-dom";
 
 const axiosSecure = axios.create({
   baseURL: "http://localhost:3000",
@@ -6,6 +9,26 @@ const axiosSecure = axios.create({
 });
 
 const useAxiosSecure = () => {
+  const { logOut } = useAuth();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    axiosSecure.interceptors.response.use(
+      (response) => {
+        return response;
+      },
+      (error) => {
+        console.log("Error tracked in the interceptor:", error.message);
+        if (error.response.status === 401 || error.response.status === 403) {
+          logOut()
+            .then((result) => {
+              navigate("/login");
+            })
+            .catch((error) => console.log(error));
+        }
+      }
+    );
+  }, []);
 
   return axiosSecure;
 };
